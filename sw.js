3ec0abef709510a16afbc0ee05f7bc33de20309e
wajihhassan98-arch <wajihhassan98@gmail.com>
2026-08-07 22:15:16 -0500

    Playback in detection mode; clearer iOS explanation (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "urdu-v6";
+const CACHE = "urdu-v7";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png"];
 
 self.addEventListener("install", (e) => {
